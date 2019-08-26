58b02f7f5b5d0378f4329f3d021969d7455c4ce4
xieli <xieli@xiaomi.com>
2019-08-26 15:31:23 +0800

    Initial commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @author 谢力
+ * @Description
+ * @Date 创建于 2019/8/26 15:27
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
